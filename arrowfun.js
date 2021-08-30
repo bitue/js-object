c74599a715069cbe2f117f26c81d2006c30c229b
bitue <66092560+bitue@users.noreply.github.com>
2021-08-30 18:24:15 +0600

    Update arrowfun.js

--- arrowfun.js
+++ arrowfun.js
@@ -3,5 +3,6 @@ const name = () => {
 }
 
 document.getElementById("add").addEventListener('click', ()=>{
+    //here this is a arrow func and you will pass data but not custimized data as well to pass
     
 })
